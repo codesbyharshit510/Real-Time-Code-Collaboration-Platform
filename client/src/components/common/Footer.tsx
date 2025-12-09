356e88c313b9eeaf84a6d2fb2b87caeb2abcf438
Harshit Rai <harshit143rai@gmail.com>
2025-12-09 21:57:50 +0530

    Final commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{"Harshit Rai "}
+                <a
+                    href="https://github.com/codesbyharshit510"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Harshit Rai
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
