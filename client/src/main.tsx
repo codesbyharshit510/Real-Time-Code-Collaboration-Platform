e090c495107e90c5abd2ae2b87398fb552e5f789
Harshit Rai <harshit143rai@gmail.com>
2025-12-12 21:11:03 +0530

    Fix deployment: Remove broken vercel.json rewrite rule

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 // import React from "react"
-import "tldraw/tldraw.css"
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import AppProvider from "./context/AppProvider.tsx"
-import "tldraw/tldraw.css"
-import "@/styles/global.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import AppProvider from './context/AppProvider.tsx'
+import "tldraw/tldraw.css" // <--- CRITICAL: Tldraw styles must be here
+import "@/styles/global.css" // <--- Your global styles
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     // <React.StrictMode>
